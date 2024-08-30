1123db3a79a4eba401b795f316df961aa4282409
DreamingCodes <me@dreaming.codes>
2024-08-30 16:40:31 +0200

    updated build.rs

--- build.rs
+++ build.rs
@@ -7,15 +7,17 @@ use std::path::PathBuf;
 fn main() {
     // Compile the C library
     cc::Build::new()
+        .file("c_library/sensirion_common.c")
+        .file("c_library/sensirion_i2c_hal.c")
         .file("c_library/sht4x_i2c.c")
-        .include("c_library/..")
-        .compile("sht4x");
+        .include("c_library")
+        .compile("sensirion");
 
     // Generate bindings
     let bindings = bindgen::Builder::default()
-        .header("c_library/sht4x_i2c.h")
-        .clang_arg("-Ic_library/..")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .header("wrapper.h")
+        .clang_arg("-Ic_library")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
